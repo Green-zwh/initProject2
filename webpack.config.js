eb90c4ce293525aa5c07de6cc77a8754ba0cd978
zwh <834712578@qq.com>
2022-03-22 14:28:02 +0800

    配置了根目录@

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
 module.exports = {
+    lintOnSave: false,
+    publicPath: './',
+    chainWebpack: config => {
+        // 配置别名后需要重启项目
+        config.resolve.alias.set('@', resolve('src'))
+    },
     module: {
-        rules: [
-            {
+        rules: [{
                 test: /\.less$/,
                 loader: "style-loader!css-loader!less-loader",
             }
-    
+
         ]
-    
+
     }
 }
